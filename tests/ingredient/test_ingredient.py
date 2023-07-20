87829de22fc8dcab62fc515c374de460d0525361
Wallace Rodrigo <wallacerodrigo2004@gmail.com>
2023-07-20 14:38:19 -0300

    req 1 - ingredients tests

--- tests/ingredient/test_ingredient.py
+++ tests/ingredient/test_ingredient.py
@@ -3,4 +3,13 @@ from src.models.ingredient import Ingredient  # noqa: F401, E261, E501
 
 # Req 1
 def test_ingredient():
-    pass
+    meat = Ingredient("carne")
+    invalid_ingredient = Ingredient("xablau")
+    salmon = Ingredient("salmão")
+
+    assert meat.name == "carne"
+    assert repr(meat) == "Ingredient('carne')"
+    assert hash(meat) == hash("carne")
+    assert invalid_ingredient.restrictions == set()
+    assert meat != salmon
+    assert meat == meat
