1abcd5c9779aaacdc28f6c990b6872c6c5bd675a
Wallace Rodrigo <wallacerodrigo2004@gmail.com>
2023-07-21 15:14:38 -0300

    req 4 - class MenuBuilder

--- src/services/menu_builder.py
+++ src/services/menu_builder.py
@@ -1,6 +1,6 @@
 from typing import Dict, List
 
-from services.inventory_control import InventoryMapping
+# from services.inventory_control import InventoryMapping
 from services.menu_data import MenuData
 
 DATA_PATH = "data/menu_base_data.csv"
@@ -10,7 +10,7 @@ INVENTORY_PATH = "data/inventory_base_data.csv"
 class MenuBuilder:
     def __init__(self, data_path=DATA_PATH, inventory_path=INVENTORY_PATH):
         self.menu_data = MenuData(data_path)
-        self.inventory = InventoryMapping(inventory_path)
+        # self.inventory = InventoryMapping(inventory_path)
 
     def make_order(self, dish_name: str) -> None:
         try:
@@ -26,4 +26,16 @@ class MenuBuilder:
 
     # Req 4
     def get_main_menu(self, restriction=None) -> List[Dict]:
-        pass
+        main_menu = []
+
+        for dish in self.menu_data.dishes:
+            if not restriction or restriction not in dish.get_restrictions():
+                main_menu.append(
+                    {
+                        "dish_name": dish.name,
+                        "ingredients": dish.get_ingredients(),
+                        "price": dish.price,
+                        "restrictions": dish.get_restrictions(),
+                    }
+                )
+        return main_menu
